bb3f7232842e358ddfd43f230f563597a874fe10
Maksim Beliavsky <maksimbell503@gmail.com>
2022-10-02 16:31:17 +0300

    refactor task5

--- Lab1/src/task5/Task5.java
+++ Lab1/src/task5/Task5.java
@@ -1,8 +1,27 @@
 package task5;
 
+
+import java.util.Scanner;
+
 public class Task5 {
     // 1 3 5 7 6 1  0 11 13
-    public int solve(int[] sequence) {
+    public static void main(String[] args) {
+        System.out.println("Elements min number to delete:" + solve(getSequence()));
+    }
+
+    private static int[] getSequence(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter sequence length: ");
+        int arrSize = sc.nextInt();
+        int[] array = new int[arrSize];
+        for (int i = 0; i < arrSize; i++) {
+            System.out.println("Input element " + (i+1));
+            array[i] = sc.nextInt();
+        }
+        return array;
+    }
+
+    public static int solve(int[] sequence) {
         int maxAscendingLength = 0;
         int currentAscendingLength;
         boolean isAscending;
